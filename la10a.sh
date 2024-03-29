87f872ff58089d6684ef118087b11cf6eee0e9a7
gargle <638646+gargle@users.noreply.github.com>
2024-03-29 09:59:25 +0000

    explain la10a.sh better

--- la10a.sh
+++ la10a.sh
@@ -3,7 +3,9 @@
 # log and exit if there is an error
 set -ex
 
-echo read a file and display its contents
+# echo ask for a username and read a file DD_FILE_IN and display its contents
+
+# the file to display
 export DD_FILE_IN=./la10a.fil
 
 cat <<END_OF_INPUT |
